6cd70cd094bf6ac8b70981039b4de5a567b6e232
Chinmay K H <chinmayharitas@gmail.com>
2019-09-28 14:19:06 +0530

    Initial Commit

---
+++ lib/Lust.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+void main ()
+{
+  runApp(
+    new MaterialApp(
+      title : 'hhh',
+      home : new Scaffold(
+        body: ListView(children: <Widget>[
+          Text('IAMUseless'),
+          Text('Iamuseless')
+        ],),
+      )
+    )
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import './listes.dart';
+
+List <String> genre = [];
+List <List<String>> todo = [[]];
+
+void main ()
+{
+TextEditingController _textFieldController = TextEditingController();
+
+  runApp(
+    new MaterialApp(
+      title : 'TO - DO',
+      home : TextFeildAlertDialog(),
+    )
+  );
+}
+class TextFeildAlertDialog extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _TextFieldAlertDialog();
+  }
+
+}
+
+
+  class _TextFieldAlertDialog extends State<TextFeildAlertDialog> {
+  TextEditingController _textFieldController = TextEditingController();
+     _displayDialog(BuildContext context) async {
+     return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('TextField in Dialog'),
+            content: TextField(
+              controller: _textFieldController,
+              decoration: InputDecoration(hintText: "TextField in Dialog"),
+            ),
+            actions: <Widget>[
+              new FlatButton(
+                child : new Text ('Add'),
+                onPressed: ()
+                {
+                  setState(() {
+                    genre.add(_textFieldController.text);
+                    Navigator.of(context).pop();
+                    print (genre);
+                  });
+                },
+              ),
+              new FlatButton(
+                child: new Text('CANCEL'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('TO - DO'),
+        ),
+        body: Column(children: <Widget>[
+          Container(
+            margin:  EdgeInsets.all(20.0),
+            child : FloatingActionButton(
+              //child : Text('Add Genre'),
+              child: Icon(Icons.person_add),
+              onPressed: (){
+                 _displayDialog(context);
+              },
+            )
+          ),
+          Column(children: 
+            genre.map((element) => FlatButton(
+              child: Text(element), 
+              onPressed: ()
+              {
+                //SCSC
+                return TodoList();
+              },            
+              )).toList(),
+          ),
+        ],),
+      );
+  }
+  }
