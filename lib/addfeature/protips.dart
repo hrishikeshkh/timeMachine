1119ebf542280e72a94e2ea1f8c65aa25e14322c
Chinmay K H <chinmayharitas@gmail.com>
2019-11-23 12:47:34 +0530

    protipfix

--- lib/addfeature/protips.dart
+++ lib/addfeature/protips.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class ProTips extends StatelessWidget
 {
 
-  List <String> tips = [];
+  List <String> tips = ['chem is volatile' , 'not brought si eqavalent to not done'];
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
