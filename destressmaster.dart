69a7b3334e08b8f58f6c53799a298c1bd6cc570b
hrishikeshkh <49692394+hrishikeshkh@users.noreply.github.com>
2020-01-15 13:03:18 +0530

    destress

---
+++ destressmaster.dart
@@ -0,0 +1,50 @@
+import 'package:beta_time_machine/destress/snake/main.dart';
+import 'package:beta_time_machine/destress/tic-tac-toe/ticmain.dart';
+import 'package:flutter/material.dart';
+
+class DestressMaster extends StatelessWidget
+{
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome To Destress...'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Center(
+            child : Container(
+              padding: EdgeInsets.only(top: 10.0),
+              child : Card(
+            child: Text('We Trust you not to overdo this' , style: TextStyle(fontSize: 20.0),),
+          ))),
+          
+              Text (' '),
+              Card(
+              child :  ListTile(
+                  title : Text('TIC - TAC - TOE'),
+                  leading: Icon(Icons.apps),
+                  onTap: ()
+                  {
+                    Navigator.push(context , MaterialPageRoute(builder: (context) => MyApptic() ));
+                  },
+                
+              )),
+              Card (
+              child :  ListTile (
+                title : Text ('SNAKEEE'),
+                leading: Icon(Icons.picture_in_picture),
+                onTap: ()
+                {
+                    Navigator.push(context , MaterialPageRoute(builder: (context) => Homesn() ));
+                },
+              )),
+           Container(child: Image.asset('././assets/images/des.jpeg') , padding: EdgeInsets.only(top : 10.0),)
+        ],
+      ),
+    );
+  }
+  
+}
---
+++ protips.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class ProTips extends StatelessWidget
+{
+
+  List <String> tips = [
+    'Chemistry is volatile, keep revising it' , 
+    'Not brought is equivalent to not done', 
+    'The key to learning something well is repetition; the more times you go over the material the better chance you have of storing it permanently.', 
+    'Visual learners study best when the material is graphic',
+    'Do your homework whether you have to turn it in or not. Homework helps build the concept better',
+    'The most important practice is note taking, lectures can only be restored in the brain if we write it down once and read that writing to recreate the lecture in our mind.',
+    'For chemistry, read whatever is in bold face first and then go back and read all the text, highlighting the most important information. To memorize chemical reactions,  use index cards. you  can take them wherever go and look at them whenever youu have free time',
+    'When reading science books, make sure you read the examples. If you don\'t read the examples you\'ll be lost during class.',
+    'Working out or playing sports relieves stress. You should have incentives after your homework is done: go out, talk with a friend, read for pleasure, have fun.'
+    ];
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        appBar: AppBar(
+          title: (Text('Pro - Tips')) ,
+        ),
+        body: Container( 
+          child : ListView(
+          children : tips.map((element)=> 
+          Column(
+            children : <Widget>[
+          Container(
+            padding: EdgeInsets.all(10.0),
+            color: Colors.white,
+          child :ListTile(
+            //leading: Container(child : Text('$element[1]') , color: Colors.white,),
+            title: Text('$element' , style : TextStyle(color: Colors.black , fontSize: 16)),
+            //trailing: Text('$element[1][1]' , style : TextStyle(color : Colors.black  , fontSize : 8)),
+          )),
+          Text(' ')]
+          )).toList(),
+        ),
+    ));
+  }
+  
+}
